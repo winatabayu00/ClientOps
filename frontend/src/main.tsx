f77a63fb2b3ab5b9b8f254fc482e5a5374cadc1c
winatabayu <winatabayu01@gmail.com>
2026-08-23 13:07:33 +0700

    refactor(frontend): wire app composition

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { initErrorShim, ErrorBoundary } from './errorshim'
+import { Toaster } from './components/ui/toast'
 import './styles.css'
 import App from './App'
 
@@ -9,5 +10,6 @@ initErrorShim()
 createRoot(document.getElementById('root')!).render(
   <ErrorBoundary>
     <BrowserRouter><App /></BrowserRouter>
+    <Toaster />
   </ErrorBoundary>,
 )
